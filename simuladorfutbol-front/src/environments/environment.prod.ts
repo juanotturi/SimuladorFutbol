813222dff3052a6575a876e3b36a227cfc0e447f
juanotturi <juanotturi@gmail.com>
2025-08-11 20:56:54 -0300

    Correccion en assets de environment.prod.ts

--- simuladorfutbol-front/src/environments/environment.prod.ts
+++ simuladorfutbol-front/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://simuladorfutbol-production.up.railway.app',
-  assetsBaseUrl: 'https://simuladorfutbol-production.up.railway.app'
+  assetsBaseUrl: 'https://simuladorfutbol-production.up.railway.app/assets'
+
 };
