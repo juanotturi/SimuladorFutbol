4d69396fd4dbfa6a2e70b8be07dd101c01505219
juanotturi <juanotturi@gmail.com>
2025-08-11 20:39:10 -0300

    Se agrego environment.prod.ts

---
+++ simuladorfutbol-front/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://simuladorfutbol-production.up.railway.app',
+  assetsBaseUrl: 'https://simuladorfutbol-production.up.railway.app'
+};
